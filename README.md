148f0b31b58f016aafd66bf2ce19efe301f59960
Michael Loney <mloney@protonmail.com>
2019-06-05 21:42:38 -0500

    Use argparse, support id by size

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Renames and moves files into the same structure as the source directory based on
 ./organize.py <source dir> <target dir> [--id md5|size]
 
 --id
+
     The method by which the files are recgonized. md5 uses the md5 hash of the files, size uses the size in bits of the files. Size is much faster, however if two files are the same size then it will not work. md5 is more reliable.
--- organize.py
+++ organize.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 """
-Reorganizes the target directory to match the file structure and names as the source directory based on checksums
+Reorganizes the target directory to match the file structure and names as the source directory based on checksums.
 Usage: ./organize.py <source dir> <target dir>
 """
 from os import walk, makedirs, listdir, rmdir
-from os.path import join, isfile, exists as path_exists, dirname
+from os.path import join, isfile, exists as path_exists, dirname, getsize
 from shutil import move
 from sys import argv, exit
 from hashlib import md5
+import argparse
 
-if len(argv) != 3:
-    print('Usage: ./organize.py <source dir> <target dir>')
-    exit()
-
-def organize():
+def organize(args, size):
     source_files = {}
 
-    for folder in argv[1:3]:
+    for folder in [args.source, args.target]:
         for subdir, dirs, files in walk(folder):
             for f in files:
-                md5 = hash(join(subdir, f))
+                # Set id to either the size of the file or the md5 hash
+                if size:
+                    id = getsize(join(subdir, f))
+                else:
+                    id = hash(join(subdir, f))
                 if folder == argv[1]:
                     # Add files from source to an array
-                    source_files[md5] = ({'path': join(subdir[len(argv[1]):], f)})
+                    source_files[id] = ({'path': join(subdir[len(argv[1]):], f)})
                 else:
                     # Check if hash exists in source_files. If yes -> move and rename to match source
-                    if md5 in source_files:
+                    if id in source_files:
                         current_path = join(subdir,f)
-                        new_path = join(argv[2], source_files[md5]['path'])
+                        new_path = join(argv[2], source_files[id]['path'])
                         print(current_path + ' --> ' + new_path)
                         if current_path != new_path:
                             # Check if file already exists in new_path. If yes -> rename it
@@ -43,6 +44,17 @@ def organize():
                             if len(listdir(subdir)) == 0:
                                 rmdir(subdir)
 
+def checkForSizeConflicts(args):
+    for folder in [args.source, args.target]:
+        sizes = {}
+        for subdir, dirs, files in walk(folder):
+            for f in files:
+                fpath = join(subdir, f)
+                size = getsize(fpath)
+                if size in sizes:
+                    return [fpath, sizes[size]]
+                sizes[size] = fpath
+
 """
 Computes the md5 hash of the given file
 """
@@ -53,4 +65,22 @@ def hash(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
-organize()
+
+parser = argparse.ArgumentParser(description='Reorganizes the target directory to match the file structure and names as the source directory based on checksums.')
+parser.add_argument('source', type=str, help='the dir to get copy the file structure from.')
+parser.add_argument('target', type=str, help='the dir to recreate the file structure in.')
+parser.add_argument('-i', '--id', type=str, default='md5', choices=['md5', 'size'], help='The method to id files. md5 id\'s files by their md5 hash and size by their size. First run -s to check if size will work.')
+parser.add_argument('-s', '--sizecheck', action='store_true', help='Check if the size id will work. Do not change any files.')
+args = parser.parse_args()
+
+size = args.id == 'size'
+if size or args.sizecheck:
+    conflict = checkForSizeConflicts(args)
+    if conflict:
+        print('Size conflicts exist. Size id will not work.\nConflicting files: '+str(conflict))
+        exit()
+    else:
+        print('No size conflicts exist.')
+if not args.sizecheck:
+    organize(args, size)
+
